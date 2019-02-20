39210315cc8fb206802256631afad551679a9938
bob <2106397m@student.gla.ac.uk>
2019-02-20 23:26:26 +0000

    index url/view, template_dir, media_dir, static_dir

--- World_Of_Food/urls.py
+++ World_Of_Food/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from breakfast import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^breakfast/', include('breakfast.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
