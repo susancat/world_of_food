b713f193a1749d43641b5266a5ad6e59df3cad39
Irene Zhang <46045277+susancat@users.noreply.github.com>
2019-02-22 02:33:41 +0000

    models.py and admin.py files updated

--- breakfast/models.py
+++ breakfast/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Continent(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Continent, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    continent = models.ForeignKey(Continent)
+    description = models.TextField
+    ingredients = models.TextField
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    image = models.ImageField
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Recipe, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    recipe = models.ForeignKey(Recipe)
+    review_text = models.TextField
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.recipe
+
+
+class Favourites(models.Model):
+    account = models.ForeignKey(User)
+    recipe = models.ForeignKey(Recipe)
+
+    class Meta:
+        verbose_name_plural = 'Favourites'
+
+    def __str__(self):
+        return self.account
+
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    account = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(max_length=256)
+    password = models.CharField(max_length=16)
+    # I changed the field type of favourites to URL since I thought it should save a set of recipes
+    favourites = models.URLField(max_length=256)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
--- breakfast/views.py
+++ breakfast/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'breakfast/home.html', {})
+   
+def about(request):
+    return render(request, 'breakfast/about.html', {})
+
+def contact_us(request):
+    return render(request, 'breakfast/contact_us.html', {})
+
+def sign_in(request):
+    return render(request, 'breakfast/sign_in.html', {})
+
+def sign_up(request):
+    return render(request, 'breakfast/sign_up.html', {})
+
+def my_account(request):
+    return render(request, 'breakfast/my_account.html', {})
+
+def continent_page(request):
+    return render(request, 'breakfast/continent_page.html', {})
+
+def recipe_page(request):
+    return render(request, 'breakfast/recipe_page.html')
+   
+
